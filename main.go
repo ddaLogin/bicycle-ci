ff2edab9328110ee45f15709a8ded2d8120825db
Danil Denisov <ddaLogin@gmail.com>
2020-04-08 23:29:45 +0600

    Редизайн

--- main.go
+++ main.go
@@ -66,5 +66,8 @@ func startServer(cfg Config) {
 	actions.BuildsRoutes()
 	actions.ServerRoutes()
 
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.ListenAndServe(":8090", nil)
 }
